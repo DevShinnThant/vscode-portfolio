aef37759e15bb949a614665b29e377febd693ba9
shinnthant2004 <shinthantequi@gmail.com>
2023-10-02 15:08:39 +0630

    added meta links

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,14 +69,35 @@ const CustomHead = () => {
         name="keywords"
         content="shinn thant, web developer,vscode portfolio, nextjs, shinn thant portfolio, vscode-portfolio"
       />
-      <meta property="og:title" content="Shinn Thant's Portfolio" />
       <meta
         property="og:description"
         content="A front-end developer building websites that you'd like to use."
       />
-      <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
-      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="og:title" content="Shinn Thant's Portfolio" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://devshinnthant.vercel.app/" />
+      <meta
+        property="og:description"
+        content="A front-end developer building websites that you'd like to use."
+      />
+      <meta
+        property="og:image"
+        content="https://i.postimg.cc/Sx4TkjqW/devshinn-portforlio.png"
+      />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://devshinnthant.vercel.app/"
+      />
+      <meta property="twitter:title" content="Shinn Thant | FrontEnd Dev" />
+      <meta
+        property="twitter:description"
+        content="Experienced front-end developer skilled in building robust web applications."
+      />
+      <meta
+        property="twitter:image"
+        content="https://i.postimg.cc/Sx4TkjqW/devshinn-portforlio.png"
+      />
     </Head>
   );
 };
