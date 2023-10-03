507a829cec977dbd8996a22c7c156bc9973e44fa
shinnthant2004 <shinthantequi@gmail.com>
2023-10-03 17:10:01 +0630

    favi icon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,20 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>Shinn Thant</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <meta
           name="description"
           content="Shinn Thant is a front end developer building websites and applications you'd love to use"
