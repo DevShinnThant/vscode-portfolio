1a492129c9115fb7fab8d72ece0b98da7adc375c
shinnthant2004 <shinthantequi@gmail.com>
2024-08-02 21:34:07 +0630

    refactor(app): home content text contrastion

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -6,23 +6,23 @@ export default function Home() {
     <main className="w-full h-full flex items-center">
       <div className="flex justify-center items-center w-full h-full">
         <div className="w-[500px] pl-4 h-[500px] flex flex-col justify-center items-start">
-          <p className="text-accentColor font-semibold text-[14px]">
+          <p className="text-gray-300 font-semibold text-[14px]">
             Hello, my name is
           </p>
-          <p className="text-white h-[30px] mt-4 font-semibold text-[28px] leading-[1px]">
+          <p className="text-accentColor h-[30px] mt-8 font-semibold text-4xl leading-[1px]">
             Shinn Thant.
           </p>
-          <p className="mt-3 h-[30px] text-gray-500 font-semibold text-[30px] leading-[1px]">
+          <p className="mt-3 h-[30px] text-gray-300 font-semibold text-4xl leading-[1px]">
             I love exploring new things!
           </p>
-          <p className="mt-[4px] text-[14px] tracking-[1px]  text-gray-500 ">
+          <p className="mt-4 text-[14px] tracking-[1px] text-gray-400">
             🚀 &#160;&#160; I specialize in transforming ideas into
             pixel-perfect, user-friendly websites that not only look stunning
             but also function flawlessly. With a strong foundation in HTML, CSS,
             and JavaScript, I bring creativity and technical expertise to every
             project.
           </p>
-          <p className="mt-[6px] text-[14px] text-gray-500">
+          <p className="mt-6 text-[14px] text-gray-400">
             I enjoy learning new skills and implementing them in real life.
           </p>
           <Link
